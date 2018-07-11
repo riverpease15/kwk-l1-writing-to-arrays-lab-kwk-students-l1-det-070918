e59c0c168e7a206bd6f34a858b28f1ff27037a2d
River Pease <riverpease15@gmail.com>
2018-07-11 20:19:00 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -14,6 +14,7 @@ end
 
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+  rainbow_colors << "green"
+  rainbow_colors << "blue"
+  return rainbow_colors
 end
-
-change_rainbow_colors

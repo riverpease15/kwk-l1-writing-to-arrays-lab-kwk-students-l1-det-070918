229c5648fef7f126f6b769547eeb645926d474c0
River Pease <riverpease15@gmail.com>
2018-07-11 20:20:30 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -6,9 +6,7 @@ rainbow_colors = [
 
 def change_rainbow_colors
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
-  rainbow_colors[0] = "red"
-  rainbow_colors[1] = "light_red"
-  rainbow_colors[2] = "light_yellow"
+  rainbow_colors[0, 1, 2] = ["red", "light_red", "light_yellow"]
   return rainbow_colors
 end
 
